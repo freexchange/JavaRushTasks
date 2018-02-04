cdb079116eed060d73a04ec705528de1dcd82e74
Denis Ya <000freexchange000@gmail.com>
2018-02-04 10:52:03 +0200

    1524_completed

--- 2.JavaCore/src/com/javarush/task/task15/task1525/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Solution.java
@@ -1,9 +1,6 @@
 package com.javarush.task.task15.task1525;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +10,19 @@ import java.util.List;
 
 public class Solution {
     public static List<String> lines = new ArrayList<String>();
+    static{
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(Statics.FILE_NAME)));
+            String temp;
+            while((temp = reader.readLine()) != null) {
+                lines.add(temp);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(lines);
