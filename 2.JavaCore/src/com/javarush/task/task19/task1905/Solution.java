660577546464bf04aa4283fe1e1da67345b06b81
Denis Ya <000freexchange000@gmail.com>
2018-05-31 00:40:55 +0300

    1905

--- 2.JavaCore/src/com/javarush/task/task19/task1905/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1905/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task19.task1905;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /* 
@@ -9,13 +10,71 @@ import java.util.Map;
 
 public class Solution {
     public static Map<String,String> countries = new HashMap<String,String>();
-
+    static {
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
+    }
     public static void main(String[] args) {
 
     }
 
-    public static class DataAdapter {
+    public static class DataAdapter implements RowItem{
+        private Customer customer;
+        private Contact contact;
         public DataAdapter(Customer customer, Contact contact) {
+            this.customer = customer;
+            this.contact = contact;
+        }
+
+        @Override
+        public String getCountryCode() {
+            Iterator<Map.Entry<String, String>> iterator = countries.entrySet().iterator();
+            String country = "";
+            while (iterator.hasNext())
+            {
+                //получение «пары» элементов
+                Map.Entry<String, String> pair = iterator.next();
+                String key = pair.getKey();            //ключ
+                String value = pair.getValue();        //значение
+                if (value.equals(customer.getCountryName())) {
+                    country = key;
+                }
+            }
+
+            return country;
+        }
+
+        @Override
+        public String getCompany() {
+            return customer.getCompanyName();
+        }
+
+        @Override
+        public String getContactFirstName() {
+            String[] str = contact.getName().replace(",", "").split(" ");
+            return str[1];
+        }
+
+        @Override
+        public String getContactLastName() {
+            String[] str = contact.getName().replace(",", "").split(" ");
+            return str[0];
+        }
+
+        @Override
+        public String getDialString() {
+            String str = contact.getPhoneNumber();
+            if (str.contains("-")) {
+                str = str.replace("-", "");
+            }
+            if (str.contains("(")) {
+                str = str.replace("(", "");
+            }
+            if (str.contains(")")) {
+                str = str.replace(")", "");
+            }
+            return "callto://" + str;
         }
     }
 
